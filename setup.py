54f2d4044fd42442a50020f57382309604496987
Nico <nico.pro412@gmail.com>
2020-03-05 12:07:29 +0100

    create setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+exec(open('magicsearch/version.py').read())
+setup(
+   name='magicsearch',
+   version=__version__,
+   description='A python wrapper for Overpass API. Search the bus stop (or other points of interest) in a city and download in json format.',
+   license="LICENSE.txt",
+   long_description=open('README.md').read(),
+   author='Nicola Procopio',
+   url="https://github.com/MagicBusApp/",
+   packages=['magicsearch'],  #same as name
+   install_requires=['math', 'json', 'requests'], #external packages as dependencies
+)
