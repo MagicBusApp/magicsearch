3532f3ffd08cbbc4fd0a9a7c00bbcda6df9e95f1
nico <nico.pro412@gmail.com>
2020-03-07 13:30:16 +0100

    added pydoc

--- magicsearch/magicwrap.py
+++ magicsearch/magicwrap.py
@@ -1,7 +1,40 @@
+"""
+@date: 04/03/2020 19:36
+@owner: magicbusapp
+@author: Nicola Procopio
+@description: a class to download point of interest from openstreetmap with overpass API
+"""
 import requests
 import json
 
 class poiWrapper:
+    """
+    A class to download point of interest in a city from openstreetmap using overpass query language and overpass api. By default downloads bus stop.
+
+    Parameters:
+    --------------
+    city: str.
+        The city to download data from.
+
+    tag: str.
+        default "highway". Key  used to describe a topic, category, or type of feature.
+
+    value: str.
+        default "bus_stop". The value details the specific form of the key-specified feature.
+
+    Returns:
+    --------------
+    a python dict from a json, the overpass-api's response.
+
+    Example:
+    --------------
+    >> from magicsearch.magicwrap import poiWrapper
+    >> milano_bus_stops = poiWrapper("Milano").download()
+
+    References:
+    --------------
+    The official page about openstreetmap tags [https://wiki.openstreetmap.org/wiki/Tags]
+    """
     def __init__(self, city: str, tag: str = "highway", value: str = "bus_stop"):
         self.city = city
         self.tag = tag
--- magicsearch/utils.py
+++ magicsearch/utils.py
@@ -1,6 +1,39 @@
+"""
+@date: 04/03/2020 20:52
+@owner: magicbusapp
+@author: Nicola Procopio
+@description: functions to convert lat/lon to tile and the other way around
+"""
 import math
 
 def deg2num(lat_deg, lon_deg, zoom):
+    """
+    Transform coordinates lat/lon to tiles x/y
+
+    Parameters:
+    --------------
+    lat_deg: float.
+        latitude
+    
+    lon_deg: float.
+        longitude
+    
+    zoom: int.
+        zoom of the map. From 0 to 19.
+    
+    Returns:
+    --------------
+    a vector with x and y coordinates about tile center.
+
+    Exemple:
+    --------------
+    >> from magicsearch.utils import deg2num
+    >> deg2num(39.164, 16.224, 18)
+
+    References:
+    ---------------
+    https://wiki.openstreetmap.org/wiki/Slippy_map_tilenames#Pseudo-code
+    """
     lat_rad = math.radians(lat_deg)
     n = 2.0 ** zoom
     xtile = int((lon_deg + 180.0) / 360.0 * n)
@@ -8,6 +41,33 @@ def deg2num(lat_deg, lon_deg, zoom):
     return (xtile, ytile)
 
 def num2deg(xtile, ytile, zoom):
+    """
+    Transform x/y tile center in lat/lon coordinates
+
+    Parameters:
+    --------------
+    xtile: float.
+        x coordinate to the tile center
+    
+    ytile: float.
+        y coordinate to the tile center
+    
+    zoom: int.
+        zoom of the map. From 0 to 19.
+    
+    Returns:
+    --------------
+    a vector with lat and lon.
+
+    Exemple:
+    --------------
+    >> from magicsearch.utils import num2deg
+    >> num2deg(63, 42, 7)
+
+    References:
+    ---------------
+    https://wiki.openstreetmap.org/wiki/Slippy_map_tilenames#Pseudo-code
+    """
     n = 2.0 ** zoom
     lon_deg = xtile / n * 360.0 - 180.0
     lat_rad = math.atan(math.sinh(math.pi * (1 - 2 * ytile / n)))
